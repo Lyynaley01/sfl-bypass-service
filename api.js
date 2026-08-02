55804ddb34f4f60e9b6a0997a5f30bbeb0380dce
Lyynaley01 <justlyy256@gmail.com>
2026-08-02 07:33:01 +0700

    Update api.js

--- api.js
+++ api.js
@@ -9,7 +9,10 @@ const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));;
 app.use(express.json());
 
 const BASE_URL = "https://malz-official.biz.id/slf/";
